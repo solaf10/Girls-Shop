0fa1eae594a8bf45141ddf8e70ee2a9367b25621
solaf10 <soso103m@gmail.com>
2025-04-11 00:38:01 +0300

    v-0.1

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,26 @@
+import './Features.css'
+
+const Features = () => {
+  return (
+    <div className="features">
+          <div className="card">
+            <p className="num">15</p>
+            <p className="text">Year Experience</p>
+          </div>
+          <div className="card">
+            <p className="num">2</p>
+            <p className="text">Opened in the country</p>
+          </div>
+          <div className="card">
+            <p className="num">10k+</p>
+            <p className="text">Furniture sold</p>
+          </div>
+          <div className="card">
+            <p className="num">260+</p>
+            <p className="text">bloke designe download</p>
+          </div>
+        </div>
+  )
+}
+
+export default Features
--- src/sections/HomeHero/HomeHero.jsx
+++ src/sections/HomeHero/HomeHero.jsx
@@ -14,24 +14,6 @@ const HomeHero = () => {
           <button className="shop-btn btn blurry">Shop Now</button>
           <button className="customize-btn btn">Customize order</button>
         </div>
-        <div className="features">
-          <div className="card">
-            <p className="num">15</p>
-            <p className="text">Year Experience</p>
-          </div>
-          <div className="card">
-            <p className="num">2</p>
-            <p className="text">Opened in the country</p>
-          </div>
-          <div className="card">
-            <p className="num">10k+</p>
-            <p className="text">Furniture sold</p>
-          </div>
-          <div className="card">
-            <p className="num">260+</p>
-            <p className="text">bloke designe download</p>
-          </div>
-        </div>
       </div>
     </div>
   );
