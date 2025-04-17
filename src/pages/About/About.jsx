7b16a98ff727f3298b7d0ac102fcba0861750df6
sedra komakli <asmahanasmahan045@gmail.com>
2025-04-17 15:27:16 +0300

    finish about-hero && more-than && video sections

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import AboutHero from "../../sections/AboutSections/AboutHero/AboutHero";
+import TopGreenBar from "../../components/TopGreenBar/TopGreenBar";
+import MoreThan from "../../sections/AboutSections/MoreThan/MoreThan";
+import Video from "../../sections/AboutSections/Video/Video";
 
 const About = () => {
   return (
     <div className="about">
-      <div className="container"></div>
-    </div>
+      <TopGreenBar/>
+      <AboutHero/>
+      <MoreThan/>
+      <Video/>
+      </div>
   );
 };
 
