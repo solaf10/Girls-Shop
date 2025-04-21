d7969aaafd857aef80fee1e1359a3801527caf6e
solaf10 <soso103m@gmail.com>
2025-04-21 12:56:51 +0300

    add video

--- src/sections/AboutSections/Video/Video.jsx
+++ src/sections/AboutSections/Video/Video.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import video from '../../../../public/assets/Images/video.png'
-import './Video.css'
+import React from "react";
+import video from "../../../../public/assets/Images/video.png";
+import "./Video.css";
 const Video = () => {
   return (
-    <section className='video'>
-        <div className="container">
-            <div className="video-img">
-                <img src={video} alt="" />
-            </div>
-            
+    <section className="video">
+      <div className="container">
+        <div className="video-img">
+          <video loop="" autoplay="" muted="">
+            <source
+              src="/public/assets/Modern Interior Design For Apartment(720P_60FPS).mp4"
+              type="video/mp4"
+            />
+          </video>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
