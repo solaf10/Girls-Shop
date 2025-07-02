2093a7bcb2aaa3fa4d907cb9c3fac4eb1b43241f
solaf10 <soso103m@gmail.com>
2025-07-03 00:45:45 +0300

    show products logic and forgetPassFix

--- src/App.jsx
+++ src/App.jsx
@@ -40,9 +40,9 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/cart/complate-cart" element={<ComplateCart />} />
-        <Route path="/updatePassword" element={<UpdatePassword />} />
         <Route path="/blogs/:id" element={<BlogsDetails />} />
-        <Route path="/designer" element={<Designer />}>
+        <Route path="/designer" element={<Designer />} />
+        <Route path="/updatePassword" element={<UpdatePassword />}>
           <Route
             index
             element={
