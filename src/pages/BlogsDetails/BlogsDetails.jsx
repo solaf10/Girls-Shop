c2364d5d25f2581a62cc2f9f5c3b805f596361cd
solaf10 <soso103m@gmail.com>
2025-09-06 16:23:38 +0300

    conflicts fix

--- src/pages/BlogsDetails/BlogsDetails.jsx
+++ src/pages/BlogsDetails/BlogsDetails.jsx
@@ -25,7 +25,7 @@ const BlogsDetails = () => {
   }, []);
   useEffect(() => {
     const res = recentBlogs.filter((blog) =>
-      blog.title.toLowerCase().includes(searchedKey.toLowerCase())
+      title.toLowerCase().includes(searchedKey.toLowerCase())
     );
     setFilteredBlogs(res);
   }, [searchedKey]);
@@ -53,7 +53,7 @@ const BlogsDetails = () => {
   }, []);
 
   useEffect(() => {
-    const res = recentBlogs.filter((blog) => blog.title.includes(searchedKey));
+    const res = recentBlogs.filter((blog) => title.includes(searchedKey));
     setFilteredBlogs(res);
   }, [searchedKey]);
 
@@ -74,21 +74,172 @@ const BlogsDetails = () => {
       });
   }, [params.id]);
   return (
+    // <div className="blogs">
+    //   <TopGreenBar dynamicLink={bigTitle} />
+    //   <div className="container">
+    //     <div className="blogs-detais-container">
+    //       <div className="article-detailes">
+    //         <img className="blog-img" src={image} />
+    //         <div className="date-publisher">
+    //           <p>{PublishDate}</p>
+    //           <p>-</p>
+    //           <p>{publisher}</p>
+    //         </div>
+    //         <div className="article-body">
+    //           <h1>{bigTitle}</h1>
+    //           <p>{articleText}</p>
+    //         </div>
+    //         <div className="share-post">
+    //           <p>Share Post:</p>
+    //           <a>
+    //             <img src="/assets/Images/whatsapp.png" />
+    //           </a>
+    //           <a>
+    //             <img src="/assets/Images/facebook.png" />
+    //           </a>
+    //           <a>
+    //             <img src="/assets/Images/linkedin.png" />
+    //           </a>
+    //           <a>
+    //             <img src="/assets/Images/link.png" />
+    //           </a>
+    //           <a>
+    //             <img src="/assets/Images/email.png" />
+    //           </a>
+    //         </div>
+    //         <div className="posts-slider">
+    //           <div className="previous">
+    //             <GrFormPreviousLink className="previous-icon" />
+    //             <p>Previous Post</p>
+    //           </div>
+    //           <div className="article-body">
+    //             <h1>{bigTitle}</h1>
+    //             <p>{articleText}</p>
+    //           </div>
+    //           <div className="share-post">
+    //             <p>Share Post:</p>
+    //             <a>
+    //               <img src="/assets/Images/whatsapp.png" />
+    //             </a>
+    //             <a>
+    //               <img src="/assets/Images/facebook.png" />
+    //             </a>
+    //             <a>
+    //               <img src="/assets/Images/linkedin.png" />
+    //             </a>
+    //             <a>
+    //               <img src="/assets/Images/link.png" />
+    //             </a>
+    //             <a>
+    //               <img src="/assets/Images/email.png" />
+    //             </a>
+    //           </div>
+    //           <div className="comment-bar">
+    //             <h1>
+    //               Comment: <span>1</span>
+    //             </h1>
+    //             <div className="name-and-comment">
+    //               <div className="name-and-time">
+    //                 <p className="name">Batoul abdulHadi </p>
+    //                 <p className="time"> 5 days ago</p>
+    //               </div>
+    //               <p className="comment">
+    //                 "Such a well-written piece! Insightful and inspiring — keep
+    //                 up the amazing work!"
+    //               </p>
+    //             </div>
+    //             <div className="posts-slider">
+    //               <div className="previous">
+    //                 <GrFormPreviousLink className="previous-icon" />
+    //                 <p>Previous Post</p>
+    //               </div>
+    //               <div className="next">
+    //                 <p>Next Post</p>
+    //                 <GrFormNextLink className="next-icon" />
+    //               </div>
+    //             </div>
+    //             <div className="comment-bar">
+    //               <h1>
+    //                 Comment: <span>1</span>
+    //               </h1>
+    //               <div className="name-and-comment">
+    //                 <div className="name-and-time">
+    //                   <p className="name"> abdulHadi </p>
+    //                   <p className="time"> 5 days ago</p>
+    //                 </div>
+
+    //                 <p className="comment">
+    //                   "Such a well-written piece! Insightful and inspiring —
+    //                   keep up the amazing work!"
+    //                 </p>
+    //               </div>
+    //             </div>
+    //             <div className="write-comment-sec">
+    //               <h1>Post a comment</h1>
+    //               <div className="write-comment-form">
+    //                 <div className="phone-email-info">
+    //                   <input type="text" placeholder="Phone"></input>
+    //                   <input type="text" placeholder="E-mail"></input>
+    //                 </div>
+    //                 <div className="textarea-to-write-comment">
+    //                   <label>Your Message</label>
+    //                   <textarea />
+    //                 </div>
+    //                 <div className="agree-yo-save-your-info">
+    //                   <input className="check" type="checkbox" />
+    //                   <label>
+    //                     Save my name, email, and website in this browser for the
+    //                     next time I comment.
+    //                   </label>
+    //                 </div>
+    //                 <button className="send-comment">Send</button>
+    //               </div>
+    //             </div>
+    //           </div>
+    //           <div className="recent-posts-info">
+    //             <h3>Search</h3>
+    //             <div className="search-container">
+    //               <input
+    //                 type="text"
+    //                 value={searchedKey}
+    //                 onChange={(e) => setSearchedKey(e.target.value)}
+    //               />
+    //               <CiSearch className="search-icon" />
+    //             </div>
+    //             <div className="recent-posts">
+    //               <h2 className="recent-blogs-text">Recent Posts</h2>
+    //               <div className="recent-blogs">
+    //                 {filteredBlogs.map((recent) => (
+    //                   <RecentBlog
+    //                     key={recent.id}
+    //                     id={recent.id}
+    //                     title={recent.title}
+    //                     date={recent.date}
+    //                     publisher={recent?.publisher}
+    //                   />
+    //                 ))}
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
     <div className="blogs">
-//       <TopGreenBar secondLink="../../pages/Blogs" secondPageName="Blogs" />
-      <TopGreenBar dynamicLink={blog.bigTitle} />
+      <TopGreenBar dynamicLink={bigTitle} />
       <div className="container">
         <div className="blogs-detais-container">
           <div className="article-detailes">
-            <img className="blog-img" src={blog.image} />
+            <img className="blog-img" src={image} />
             <div className="date-publisher">
-              <p>{blog.PublishDate}</p>
+              <p>{PublishDate}</p>
               <p>-</p>
-              <p>{blog.publisher}</p>
+              <p>{publisher}</p>
             </div>
             <div className="article-body">
-              <h1>{blog.bigTitle}</h1>
-              <p>{blog.articleText}</p>
+              <h1>{bigTitle}</h1>
+              <p>{articleText}</p>
             </div>
             <div className="share-post">
               <p>Share Post:</p>
@@ -113,27 +264,10 @@ const BlogsDetails = () => {
                 <GrFormPreviousLink className="previous-icon" />
                 <p>Previous Post</p>
               </div>
-              <div className="article-body">
-                <h1>{bigTitle}</h1>
-                <p>{articleText}</p>
+              <div className="next">
+                <p>Next Post</p>
+                <GrFormNextLink className="next-icon" />
               </div>
-              <div className="share-post">
-                <p>Share Post:</p>
-                <a>
-                  <img src="/assets/Images/whatsapp.png" />
-                </a>
-                <a>
-                  <img src="/assets/Images/facebook.png" />
-                </a>
-                <a>
-                  <img src="/assets/Images/linkedin.png" />
-                </a>
-                <a>
-                  <img src="/assets/Images/link.png" />
-                </a>
-                <a>
-                  <img src="/assets/Images/email.png" />
-                </a>
             </div>
             <div className="comment-bar">
               <h1>
@@ -144,60 +278,34 @@ const BlogsDetails = () => {
                   <p className="name">Batoul abdulHadi </p>
                   <p className="time"> 5 days ago</p>
                 </div>
+
                 <p className="comment">
                   "Such a well-written piece! Insightful and inspiring — keep up
                   the amazing work!"
                 </p>
               </div>
-              <div className="posts-slider">
-                <div className="previous">
-                  <GrFormPreviousLink className="previous-icon" />
-                  <p>Previous Post</p>
+            </div>
+            <div className="write-comment-sec">
+              <h1>Post a comment</h1>
+              <div className="write-comment-form">
+                <div className="phone-email-info">
+                  <input type="text" placeholder="Phone"></input>
+                  <input type="text" placeholder="E-mail"></input>
                 </div>
-                <div className="next">
-                  <p>Next Post</p>
-                  <GrFormNextLink className="next-icon" />
+                <div className="textarea-to-write-comment">
+                  <label>Your Message</label>
+                  <textarea />
                 </div>
+                <div className="agree-yo-save-your-info">
+                  <input className="check" type="checkbox" />
+                  <label>
+                    Save my name, email, and website in this browser for the
+                    next time I comment.
+                  </label>
+                </div>
+                <button className="send-comment">Send</button>
               </div>
-              <div className="comment-bar">
-                <h1>
-                  Comment: <span>1</span>
-                </h1>
-                <div className="name-and-comment">
-                  <div className="name-and-time">
-                    <p className="name"> abdulHadi </p>
-                    <p className="time"> 5 days ago</p>
-                  </div>
-
-                  <p className="comment">
-                    "Such a well-written piece! Insightful and inspiring — keep
-                    up the amazing work!"
-                  </p>
-                </div>
-              </div>
-              <div className="write-comment-sec">
-                <h1>Post a comment</h1>
-                <div className="write-comment-form">
-                  <div className="phone-email-info">
-                    <input type="text" placeholder="Phone"></input>
-                    <input type="text" placeholder="E-mail"></input>
-                  </div>
-                  <div className="textarea-to-write-comment">
-                    <label>Your Message</label>
-                    <textarea />
-                  </div>
-                  <div className="agree-yo-save-your-info">
-                    <input className="check" type="checkbox" />
-                    <label>
-                      Save my name, email, and website in this browser for the
-                      next time I comment.
-                    </label>
-                  </div>
-                  <button className="send-comment">Send</button>
-                </div>
-              </div>
-            </>
-            {/* )} */}
+            </div>
           </div>
           <div className="recent-posts-info">
             <h3>Search</h3>
