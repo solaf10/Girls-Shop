914b7f63fc8f501233f86b02226a2f2de2f18587
solaf10 <soso103m@gmail.com>
2025-09-08 23:28:52 +0300

    translationPopup

--- src/components/ShopHero/ShopHero.jsx
+++ src/components/ShopHero/ShopHero.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./ShopHero.css";
+import { useTranslation } from "react-i18next";
+
 export default function ShopHero() {
+  const { t } = useTranslation();
+
   return (
     <>
       <div className="holder">
         <div className="card interior">
-          <p>Interior Design</p>
+          <p>{t("mainCategories.interior")}</p>
         </div>
         <div className="card exterior">
-          <p>Exterior Design</p>
+          <p>{t("mainCategories.exterior")}</p>
         </div>
       </div>
     </>
